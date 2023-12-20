7f283a36f6444a171df5dc2f94f5f96a0870211b
Bart3kTK <bkamieniarz55@wp.pl>
2023-12-20 12:47:41 +0100

    drugi commit do tego poprzedniego

---
+++ server/src/main/java/com/example/s/Board.java
@@ -0,0 +1,73 @@
+package com.example.s;
+
+import com.example.s.pawns.Pawn;
+
+/*
+ *  @brief class wich represents board and holds pawns in 2D array
+ *          null in array represents free space on board
+ *          otherwise pawn is stored on particular position
+ * 
+ *      TODOs:
+ *      - add saving to database (by prototype pattern)
+ */
+public class Board 
+{
+    private final int rows;
+    private final int columns;
+    private final Pawn gameBoard[][];
+
+    public Board(final int rows, final int columns)
+    {
+        this.rows = rows;
+        this.columns = columns;
+        this.gameBoard = new Pawn[rows][columns];
+    }
+
+    public void putPawn(final int row, final int column, final Pawn pawn)
+    {
+        this.gameBoard[row][column] = pawn;
+    }
+
+    public void removePawn(final int row, final int column)
+    {
+        this.gameBoard[row][column] = null;
+    }
+
+    /*
+     *   clear string representation for gameBoard
+     */
+    public String toString()
+    {
+        String textRepresentation = "";
+        for (Pawn[] row : gameBoard) 
+        {
+            for (Pawn pawn : row) 
+            {
+                if (pawn == null)
+                {
+                    textRepresentation += "----- ";
+                    continue;
+                }
+                textRepresentation += pawn.getColor() + " ";
+            }
+            textRepresentation += "\n";
+        }
+
+        return textRepresentation;
+    }
+
+    public boolean isPositionFree(final int row, final int column)
+    {
+        if (gameBoard[row][column] == null)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public Pawn[][] getGameBoard()
+    {
+        return this.gameBoard;
+    }
+
+}
---
+++ server/src/main/java/com/example/s/pawns/Pawn.java
@@ -0,0 +1,24 @@
+package com.example.s.pawns;
+
+
+/*
+ *  @brief The class represents default Pawn (pionek xd) 
+ *  The Black and white pawn extends from this class. It allows to use factory pattern.
+ * 
+ *      TO DOs:
+ *      - implement composit pattern to store neighbors
+ *      - implement factory method pattern to make proper pawns 
+ */
+public abstract class Pawn 
+{
+    protected String color;
+    protected int row;
+    protected int column;
+
+    public String getColor()
+    {
+        return this.color;
+    }
+
+    
+}
